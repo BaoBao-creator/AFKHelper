d0d5e3d701e5fb2c94ae78badd7b1166ee19a120
BaoBao-creator <ba0ba0113377@gmail.com>
2026-06-22 15:32:03 +0700

    Fix bot join session handoff

---
+++ src/main/java/com/afk/mixin/MinecraftClientSessionAccessor.java
@@ -0,0 +1,14 @@
+package com.afk.mixin;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.util.Session;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(MinecraftClient.class)
+public interface MinecraftClientSessionAccessor {
+    @Mutable
+    @Accessor("session")
+    void afkhelper$setSession(Session session);
+}
