85d6f180fea850bf929d6f0c83e97cb69bbc066e
BaoBao-creator <ba0ba0113377@gmail.com>
2026-06-22 20:50:55 +0700

    Remove auto proxy IP functionality

--- src/main/java/com/afk/mixin/ClientConnectionMixin.java
+++ src/main/java/com/afk/mixin/ClientConnectionMixin.java
@@ -2,36 +2,16 @@ package com.afk.mixin;
 
 import com.afk.bot.BotConnection;
 import com.afk.bot.BotManager;
-import com.afk.proxy.BotProxyContext;
-import com.afk.proxy.ProxyEndpoint;
-import com.afk.proxy.ProxyInjectingChannelInitializer;
-import io.netty.bootstrap.Bootstrap;
-import io.netty.channel.ChannelHandler;
-import io.netty.channel.ChannelOption;
 import net.minecraft.network.ClientConnection;
 import net.minecraft.network.Packet;
 import net.minecraft.network.packet.s2c.play.*;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.net.InetSocketAddress;
-import java.util.Optional;
 
 @Mixin(ClientConnection.class)
 public abstract class ClientConnectionMixin {
-
-    @Redirect(
-            method = "connect",
-            at = @At(value = "INVOKE", target = "Lio/netty/bootstrap/Bootstrap;handler(Lio/netty/channel/ChannelHandler;)Lio/netty/bootstrap/AbstractBootstrap;")
-    )
-    private static io.netty.bootstrap.AbstractBootstrap<?, ?> afkhelper$injectProxyHandler(Bootstrap bootstrap, ChannelHandler handler, InetSocketAddress address, boolean useEpoll) {
-        Optional<ProxyEndpoint> proxy = BotProxyContext.claimProxyIfBotJoin(address);
-        proxy.ifPresent(endpoint -> bootstrap.option(ChannelOption.CONNECT_TIMEOUT_MILLIS, ProxyInjectingChannelInitializer.PROXY_CONNECT_TIMEOUT_MS));
-        return bootstrap.handler(proxy.<ChannelHandler>map(endpoint -> new ProxyInjectingChannelInitializer(handler, endpoint)).orElse(handler));
-    }
 
     @Inject(method = "channelRead0(Lio/netty/channel/ChannelHandlerContext;Lnet/minecraft/network/Packet;)V", at = @At("HEAD"), cancellable = true)
     private void afkhelper$routeBackgroundPacket(io.netty.channel.ChannelHandlerContext context, Packet<?> packet, CallbackInfo ci) {
--- src/main/java/com/afk/proxy/ProxyUnavailableException.java
+++
@@ -1,7 +0,0 @@
-package com.afk.proxy;
-
-public final class ProxyUnavailableException extends RuntimeException {
-    public ProxyUnavailableException(String message) {
-        super(message);
-    }
-}
